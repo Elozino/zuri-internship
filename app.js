daeddb46c7b5b7d8d375ca54535304c02689ee64
Elozino <ovedhee@gmail.com>
2022-11-05 01:20:04 +0100

    done

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const image = document.querySelector("#image");
 console.log(image);
 
 const imageArr = [
-  "./assets/image.png",
-  "./assets/image1.png",
-  "./assets/image2.png",
-  "./assets/image3.png",
-  "./assets/image4.png",
-  "./assets/image5.png",
+  "https://images.unsplash.com/photo-1661961110218-35af7210f803?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1664574654578-d5a6a4f447bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  "https://plus.unsplash.com/premium_photo-1661342567521-e42e1926807a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  "https://plus.unsplash.com/premium_photo-1661683706979-c349c21f79c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1667554278033-bd56c921314c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1667554278033-bd56c921314c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
 ];
 
 let currentSlide = 0;
